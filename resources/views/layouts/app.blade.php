2ae8a52b7dacb102c4fe24dcf1202f0f457fc06e
takuya.kusunoki <takuya.kusunoki@softfront-japan.co.jp>
2018-05-17 06:08:23 +0000

    complete

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>課題：タスク管理アプリ</title>
+    </head>
+    
+    <body>
+        @yield("content")
+    </body>
+</html>
