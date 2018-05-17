b99787c8443144027013bd0989d37745e7ff5513
takuya.kusunoki <takuya.kusunoki@softfront-japan.co.jp>
2018-05-17 08:53:39 +0000

    validation,add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -33,7 +33,13 @@ class TasksController extends Controller
     //postでtasks/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
+        $this->validate($request, [
+            "status" => "required|max:10",      //追加
+            "content" => "required|max:191",    //追加
+        ]);
+        
         $task = new Task;
+        $task->status = $request->status;        //追加
         $task->content = $request->content;
         $task->save();
         
@@ -63,7 +69,13 @@ class TasksController extends Controller
     //putまたはpatchでtasks/idにアクセスされた場合の「更新処理」
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            "status" => "required|max:10",      //追加
+            "content" => "required|max:191",    //追加
+        ]);
+        
         $task = Task::find($id);
+        $task->status = $request->status;       //追加
         $task->content = $request->content;
         $task->save();
         
