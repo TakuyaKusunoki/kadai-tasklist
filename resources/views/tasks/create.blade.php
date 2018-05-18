fb82a12ca925bb256ac2591b0985c0d42f2eb82c
takuya.kusunoki <takuya.kusunoki@softfront-japan.co.jp>
2018-05-18 05:45:13 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,16 +4,23 @@
 
 <!--ここにコンテンツを書く-->
     <h1>タスク新規作成ページ</h1>
-    
-    {!! Form::model($task, ["route" => "tasks.store"]) !!}
-        
-        {!! Form::label("status", "状態:") !!}
-        {!! Form::text("status") !!}
-        
-        {!! Form::label("content", "タスク:") !!}
-        {!! Form::text("content") !!}
-        
-        {!! Form::submit("登録") !!}
-        
-    {!! Form::close() !!}
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
+            {!! Form::model($task, ["route" => "tasks.store"]) !!}
+                
+                <div class="form-group">
+                    {!! Form::label("content", "タスク:") !!}
+                    {!! Form::text("content", null, ["class" => "form-control"]) !!}
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::label("status", "状態:") !!}
+                    {!! Form::text("status", null, ["class" => "form-control"]) !!}
+                </div>
+                
+                {!! Form::submit("登録", ["class" => "btn btn-warning"]) !!}
+                
+            {!! Form::close() !!}
+        </div>
+    </div>
 @endsection
